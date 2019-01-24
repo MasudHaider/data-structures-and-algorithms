967960a9ad90ae58c881de021b87a85733c16a21
MasudHaider <masud6563@gmail.com>
2019-01-24 11:00:08 +0600

    Tower of Hanoi (recursion)

--- recursion.cpp
+++ recursion.cpp
@@ -1,18 +1,20 @@
 #include<iostream>
-
+using namespace std;
+int moves = 0;
 void TowerOfHanoi(int n, char from, char to, char via)
 {
+    moves++;
     //if only 1 disk, make the move and return
     if(n == 1)
     {
-        std::cout<<"Move disk 1 from peg "<<from<<" to peg "<<to<<std::endl;
+        cout<<"Move disk 1 from peg "<<from<<" to peg "<<to<<endl;
         return;
     }
     //Move top n-1 disks from A to B, using C as auxiliary
     TowerOfHanoi(n-1, from, via, to);
 
     //Move remaining disks from A to C
-    std::cout<<"\nMove disk "<< n <<" from peg "<<from<<" to peg "<< to <<std::endl;
+    cout<<"\nMove disk "<< n <<" from peg "<<from<<" to peg "<< to <<endl;
 
     //Move n-1 disks from B to C, using A as auxiliary
     TowerOfHanoi(n-1, via, to, from);
@@ -20,6 +22,11 @@ void TowerOfHanoi(int n, char from, char to, char via)
 
 int main()
 {
-    TowerOfHanoi(4,'A','B','C');
+    int discs;
+    cout << "Enter the number of discs: " << endl;
+    cin >> discs;
+    TowerOfHanoi(discs, 'A', 'B', 'C');
+    cout << "It took " << moves << " moves. " << endl;
+
     return 0;
 }
